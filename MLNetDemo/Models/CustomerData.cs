cffb8c7de6f0ea16a85e422657af82e4af40ad1d
kittikawin <kittikawin_ball@hotmail.com>
2025-08-01 23:02:34 +0700

    Clean code and update CustomerData.cs model

--- MLNetDemo/Models/CustomerData.cs
+++ MLNetDemo/Models/CustomerData.cs
@@ -4,11 +4,11 @@ namespace MLNetDemo.Models;
 
 public class CustomerData
 {
-    [LoadColumn(0), ColumnName("Age")] public float Age { get; set; }
+    [LoadColumn(0)] public float Age { get; set; }
 
-    [LoadColumn(1), ColumnName("Income")] public float Income { get; set; }
+    [LoadColumn(1)] public float Income { get; set; }
 
-    [LoadColumn(2), ColumnName("PreviousPurchases")] public float PreviousPurchases { get; set; }
+    [LoadColumn(2)] public float PreviousPurchases { get; set; }
 
-    [LoadColumn(3), ColumnName("Label")] public bool Label { get; set; }
+    [LoadColumn(3)] public bool Label { get; set; }
 }
--- MLNetDemo/Program.cs
+++ MLNetDemo/Program.cs
@@ -1,33 +1,30 @@
 ﻿using Microsoft.ML;
 using MLNetDemo.Models;
 
-var context = new MLContext();
+var context = new MLContext(seed: 1);
 
 // Load data
 var data = context.Data.LoadFromTextFile<CustomerData>("data.csv", hasHeader: true, separatorChar: ',');
 
 // Split for training and testing
-var split = context.Data.TrainTestSplit(data, testFraction: 0.2);
+var trainTestSplit = context.Data.TrainTestSplit(data, testFraction: 0.2);
+var trainingData = trainTestSplit.TrainSet;
 
 // Define the pipeline
-var pipeline =
+var dataProcessPipeline =
     context.Transforms.Concatenate("Features",
-            nameof(CustomerData.Age),
-            nameof(CustomerData.Income),
-            nameof(CustomerData.PreviousPurchases))
-        .Append(context.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label",
-            featureColumnName: "Features"));
+        nameof(CustomerData.Age),
+        nameof(CustomerData.Income),
+        nameof(CustomerData.PreviousPurchases));
+
+var trainer = context.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features");
+var trainingPipeline = dataProcessPipeline.Append(trainer);
 
 // Train the model
-var model = pipeline.Fit(split.TrainSet);
-
-var predictions = model.Transform(split.TestSet);
-var metrics = context.BinaryClassification.Evaluate(predictions);
-
-Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
+var trainedModel = trainingPipeline.Fit(trainingData);
 
 // Make a prediction
-var predictionEngine = context.Model.CreatePredictionEngine<CustomerData, PurchasePrediction>(model);
+var predictionEngine = context.Model.CreatePredictionEngine<CustomerData, PurchasePrediction>(trainedModel);
 
 var sample = new CustomerData { Age = 28, Income = 55000, PreviousPurchases = 1 };
 var prediction = predictionEngine.Predict(sample);
